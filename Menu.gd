e7e40a959e756e0165b09560e7ca6766f096b322
Antonio Paulo Barros <95260401+apabsp@users.noreply.github.com>
2024-05-11 11:31:04 -0300

    sistema de interação e correr adicionado

--- Menu.gd
+++ Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://cena.tscn")
+	get_tree().change_scene_to_file("res://Cenas/cena.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
