cde9a9c99b45209124e8d6d29c891d137fd4eab8
FelipeNunes <felipe.nunes.morgado@gmail.com>
2024-05-11 15:34:38 -0300

    Revert "oi"
    This reverts commit 899992c2eb363c38e2e2c9eaf323f74be16af05d.

--- Cenas/Mirror.gd
+++
@@ -1,6 +0,0 @@
-class_name Interacao_2 extends Area2D
-
-@export var interact_label = ""
-@export var interact_type = "Miror" # get item or read thing or press thing
-@export var interact_value = 0 # 0 is default i guess. dfont think thwis will be useful
-
--- INTERACTIONOBJECT.gd
+++ INTERACTIONOBJECT.gd
@@ -1,6 +1,6 @@
-class_name Interacao_1 extends Area2D
+class_name Interactable extends Area2D
 
-@export var interact_label = ""
-@export var interact_type = "Lock" # get item or read thing or press thing
+@export var interact_label = "esqueci de settar um label para esse objeto!"
+@export var interact_type = "none" # get item or read thing or press thing
 @export var interact_value = 0 # 0 is default i guess. dfont think thwis will be useful
 
